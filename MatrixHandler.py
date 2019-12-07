84cb0185587cb31fba8bdb29db04ed2cfd9e9641
Fokion Sotiropoulos <fokion.s@gmail.com>
2019-12-07 19:48:03 +0000

    day 3 done

--- MatrixHandler.py
+++ MatrixHandler.py
@@ -1,51 +1,34 @@
-import  numpy as np
+
 class MatrixHandler:
+    DX = {'L': -1, 'R': 1, 'U': 0, 'D': 0}
+    DY = {'L': 0, 'R': 0, 'U': 1, 'D': -1}
 
-    def __init__(self, matrix,starting_point_x,starting_point_y):
-        self.matrix = matrix
-        self.intersections = set()
-        self.last_x = starting_point_x
-        self.last_y = starting_point_y
-        self.wiresAdded = 0
-        self.matrix[self.last_x, self.last_y] = "O"
-
-    def mark_board_with_fixed_y(self, y, from_x, to_x):
-        for x in range(from_x, to_x):
-            if self.matrix[x, y] != " ":
-                self.matrix[x, y] = "X"
-                self.intersections.add(str(x) + "," + str(y))
-            elif self.matrix[x, y] == " ":
-                self.matrix[x, y] = "|"
-
-    def mark_board_with_fixed_x(self, x, from_y, to_y):
-        for y in range(from_y, to_y):
-            if self.matrix[x, y] != " ":
-                self.matrix[x, y] = "X"
-                self.intersections.add(str(x) + "," + str(y))
-            elif self.matrix[x, y] == " ":
-                self.matrix[x, y] = "-"
+    def __init__(self):
+      pass
 
     def add_wire(self,commands):
-        self.wiresAdded+=1
+        y = 0
+        x = 0
+        wiring = {}
+        length = 0
         for command in commands:
-            type_of_movement = str(command[0])
-            number_of_moves = int(command[1:])
-            if type_of_movement == "R":
-                self.mark_board_with_fixed_x(self.last_x, self.last_y+1, (self.last_y + number_of_moves))
-                self.last_y += number_of_moves
-            elif type_of_movement == "L":
-                self.mark_board_with_fixed_x(self.last_x, (self.last_y - number_of_moves), self.last_y)
-                self.last_y -= number_of_moves
-            elif type_of_movement == "U":
-                self.mark_board_with_fixed_y(self.last_y, self.last_x+1, (self.last_x + number_of_moves))
-                self.last_x += number_of_moves
-            else:
-                self.mark_board_with_fixed_y(self.last_y, (self.last_x - number_of_moves), self.last_x)
-                self.last_x -= number_of_moves
-        self.print_state("wire__"+str(self.wiresAdded))
+            direction = str(command[0])
+            steps = int(command[1:])
+            for _ in range(steps):
+                x += self.DX[direction]
+                y += self.DY[direction]
+                length += 1
+                if (x, y) not in wiring:
+                    wiring[(x, y)] = length
+        return wiring
 
-    def getIntersections(self):
-        return self.intersections
+    def get_cross(self,wire1 , wire2):
+        both = set(wire1.keys()) & set(wire2.keys())
+        return min([abs(x)+abs(y) for (x,y) in both])
 
-    def print_state(self,name):
-        np.savetxt("matrix__" + str(name) + ".txt", self.matrix, delimiter='', fmt="%s")
+    def get_intersect(self, wire1, wire2):
+        both = set(wire1.keys()) & set(wire2.keys())
+        return min([wire1[p] + wire2[p] for p in both])
+
+
+
--- daythree.py
+++ daythree.py
@@ -3,20 +3,19 @@ import  numpy as np
 from MatrixHandler import MatrixHandler
 
 
-fp = open('./test-3.txt')
+fp = open('./input-day-3.txt')
 wires = fp.readlines()
-width = 1000
-height = 1000
-matrix = np.full((width, height), ' ')
-startingPoint_x = height - 500
-startingPoint_y = 300
-handler =  MatrixHandler(matrix,startingPoint_x,startingPoint_y)
-intersections = set()
-wire_index = 0
+
+handler = MatrixHandler()
+
+wirings = []
 for wire in wires:
     commands = wire.split(",")
-    handler.add_wire(commands)
-print(handler.getIntersections())
+    wirings.append(handler.add_wire(commands))
+print(handler.get_cross(wirings[0],wirings[1]))
+print(handler.get_intersect(wirings[0],wirings[1]))
+
+
 
 
 
